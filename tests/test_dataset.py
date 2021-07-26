48a7e06d025f3a0409e8fddb0921e02786636aa6
David Matos <davidmatos06@gmail.com>
2021-07-26 16:12:42 +0200

    add dataset tests and transforms

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,66 +1,46 @@
 from unittest import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pytest
+from dataset.loader import create_loader
 from dataset.open_images_dataset import OpenImagesDataset
 from dataset.parser_open_images import OpenImagesParser
+from dataset.transforms import MyCompose
 from typing_extensions import OrderedDict
 
 
-def dummy_parse(ann_file):
-    img_id = Mock()
-    return img_id
-
-
 # @patch('torch_object_detection.parser.parser_open_images.OpenImagesParser._load_annotations')
-@pytest.mark.skip(reason="need to modify")
+# @pytest.mark.skip(reason="need to modify")
 class TestNetwork(TestCase):
     def setUp(self):
-        pass
+        self.path = "/home/david/fiftyone/open-images-v6"
+        self.splits = ("train", "validation")
+        self.dataset_cls = OpenImagesDataset
+        self.datasets = OrderedDict()
+        for s in self.splits:
+            self.datasets[s] = self.dataset_cls(self.path, s)
+        self.train_dataset, self.val_dataset = list(self.datasets.values())
 
     @patch(
         "torch_object_detection.parser.parser_open_images.OpenImagesParser._load_annotations"
     )
+    @pytest.mark.skip(reason="not ready yet")
     def test_x(self, mock_parser):
-        mock_parser.side_effect = dummy_parse
+        # mock_parser.side_effect # a fcn
         OpenImagesParser("kahlua.json")._load_annotations("kahlua.json")
         mock_parser.assert_called()
 
     def test_len_dataset(self):
-        path = "/home/david/fiftyone/open-images-v6"
-        splits = ("train", "validation")
-        dataset_cls = OpenImagesDataset
-        datasets = OrderedDict()
-        for s in splits:
-            datasets[s] = dataset_cls(path, s)
-        assert len(datasets["train"].parser.img_ids) == 1000
-        assert datasets["train"].__len__() == 1000
-        assert datasets["validation"].__len__() == 200
+        assert self.train_dataset.__len__() == 1000
+        assert self.val_dataset.__len__() == 200
 
-    def test_mock_works(self):
-        annff = "/home/david/fiftyone/open-images-v6/train/labels.json"
-        OpenImagesParser(annff)._load_annotations(annff)
-        assert 1 == 1
-
-    def test_input_image_size(self):
-        pass
-        # input_size = 224  # input of image
-        # batch_size = 2
-        # num_workers = 2
-        # interpolation = "bilinear"
-        # fill_color = "mean"
-        # loader_train = create_loader(
-        #     dataset_train,
-        #     input_size,
-        #     batch_size,
-        #     interpolation=interpolation,
-        #     fill_color=fill_color,
-        #     num_workers=num_workers,
-        #     is_training=True,
-        # )
-
-    def test_augmentations(self):
-        pass
-
-    def test_shape(self):
-        pass
+    def test_loader(self):
+        assert self.train_dataset.transform is None
+        create_loader(
+            self.train_dataset,
+            input_size=224,
+            batch_size=2,
+            interpolation="bilinear",
+            is_training=True,
+        )
+        assert isinstance(self.train_dataset.transform, MyCompose)
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -2,26 +2,50 @@ import unittest
 from unittest.mock import patch
 
 import numpy as np
-from dataset.transforms import RandomResizePad
+from dataset.transforms import RandomResizePad, ResizePad
+from numpy.testing import assert_array_equal
 from PIL import Image
 
 
 class TestTransform(unittest.TestCase):
-    @patch("random.uniform")
-    def test_randomresizepad(self, mock_scale_factor):
-        ann = dict(
+    def setUp(self):
+        self.ann = dict(
             boxes=np.array(([[112, 112, 336, 336]]), dtype=np.float64),
             labels=np.array(([1])),
         )
-        input_img = np.random.random((448, 448))
-        pil_img = Image.fromarray(input_img)
+        self.input_img = np.random.random((448, 448))
+        self.pil_img = Image.fromarray(self.input_img)
+        self.target_size = 224
+
+    @patch("random.uniform")
+    def test_randomresizepad(self, mock_scale_factor):
         mock_scale_factor.return_value = 1.0
-        target_size = 224
         interpolation = "random"
         fill_color = (0, 0, 0)
+        random_interp = (Image.BILINEAR, Image.BICUBIC)
         img_tf = RandomResizePad(
-            target_size, interpolation=interpolation, fill_color=fill_color
+            self.target_size, interpolation=interpolation, fill_color=fill_color
         )
-        new_img, new_ann = img_tf(pil_img, ann)
+        new_img, new_ann = img_tf(self.pil_img, self.ann)
         assert new_img.size[0] == 224
         assert new_img.size[1] == 224
+        msg = "not expected interpolation"
+        assert_array_equal(img_tf.interpolation, random_interp, msg)
+
+        # Test different interpolation
+        interpolation = "lanczos"
+        img_tf = RandomResizePad(
+            self.target_size, interpolation=interpolation, fill_color=fill_color
+        )
+        assert img_tf.interpolation == Image.LANCZOS
+
+    def test_resizepad(self):
+        interpolation = "bicubic"
+        fill_color = (0, 0, 0)
+        img_tf = ResizePad(
+            self.target_size, interpolation=interpolation, fill_color=fill_color
+        )
+        new_img, new_ann = img_tf(self.pil_img, self.ann)
+        assert new_img.size[0] == 224
+        assert new_img.size[1] == 224
+        assert img_tf.interpolation == "bicubic"
